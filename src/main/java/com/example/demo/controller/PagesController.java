2770257559e5f6e0a6f477d8ba742c3fcb60ae7f
MaylenMDZ <mazariegosmaylenmaylen@gmail.com>
2019-12-04 02:00:09 -0600

    Update PagesController.java
    Crear metodo para traer datos de producto seleccionado.

--- src/main/java/com/example/demo/controller/PagesController.java
+++ src/main/java/com/example/demo/controller/PagesController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Pedido;
 import com.example.demo.model.Producto;
+import com.example.demo.model.Reseñas;
 import com.example.demo.repository.ContenidoRepository;
 import com.example.demo.repository.ProductoRepository;
+import com.example.demo.repository.ReseñasRepository;
 import com.example.demo.repository.tipoproductosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +14,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PagesController {
@@ -18,18 +23,16 @@ public class PagesController {
     ContenidoRepository contenidoRepository;
     @Autowired
     tipoproductosRepository tipoproductosRepository;
+    @Autowired
+    ProductoRepository productoRepository;
+    @Autowired
+    ReseñasRepository reseñasRepository;
 
     @RequestMapping("/")
 	public String home(Model model) {
 		model.addAttribute("contenido", contenidoRepository.findAll());
                 model.addAttribute("tipoproductos", tipoproductosRepository.findAll());
 		return "index";
-	}
-
-	@RequestMapping("product-detail")
-	public String ProductDetail(Model model) {
-		model.addAttribute("contenido", contenidoRepository.findAll());
-		return "product-detail";
 	}
 
 	@RequestMapping("about")
@@ -63,9 +66,19 @@ public class PagesController {
 	public String shoping_cart(Model model) {
 		model.addAttribute("contenido", contenidoRepository.findAll());
 		return "shoping-cart";
-
-
         }
+        
+        @RequestMapping(value="product-detail/{id}", method=RequestMethod.GET)
+	public String productDetail(@PathVariable("id") long id, ModelMap model) {
+            long x = id;
+            model.addAttribute("produc", new Pedido());
+            model.addAttribute("reseñ", new Reseñas());
+            model.addAttribute("productosRe", productoRepository.findByName(""));
+            model.put("productos", productoRepository.findByIdProducto(id));
+            model.put("reseñas", reseñasRepository.reseñasByIdProducto(x));
+            model.put("contenido", contenidoRepository.findAll());
+            return "product-detail";
+	}
 
    }
         
