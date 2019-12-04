da69dd25d3d2609445ee352ea1cdae2e25b15793
fanylucerohe <fanylu.201522@gmail.com>
2019-12-04 02:30:23 -0600

    Update PagesController.java

--- src/main/java/com/example/demo/controller/PagesController.java
+++ src/main/java/com/example/demo/controller/PagesController.java
@@ -48,6 +48,7 @@ public class PagesController {
 
 	@RequestMapping("product")
 	public String product(Model model) {
+		model.addAttribute("productos", productoRepository.findByAll());
 		model.addAttribute("contenido", contenidoRepository.findAll());
 		model.addAttribute("tipoproductos", tipoproductosRepository.findAll());
 		return "product";
